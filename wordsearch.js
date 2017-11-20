78c1dfbcb1324260eebfae857743bdb91eb98ab9
Danny Krause <vanreece@gmail.com>
2017-11-19 22:19:53 -0800

    first commit

---
+++ wordsearch.js
@@ -0,0 +1,77 @@
+var wordlist = [];
+var wordlistLoaded = false;
+var gw = [];
+
+function getWordlist(callback) {
+  var request = new XMLHttpRequest();
+  request.open('GET', 'scrabble.txt', true);
+  request.send(null);
+  request.onreadystatechange = function () {
+    if (request.readyState === 4 && request.status === 200) {
+      var type = request.getResponseHeader('Content-Type');
+      if (type.indexOf("text") !== 1) {
+        var wordlist = request.responseText.split('\n').slice(0,-1);
+        callback(wordlist);
+      }
+    }
+  }
+}
+
+function wordInSet(word, charString) {
+  var charSet = new Set(charString.split(''));
+  for (var i = 0; i < word.length; i++) {
+    var letter = word[i];
+    if (!charSet.has(letter)) {
+      return false;
+    }
+    charSet.delete(letter);
+  }
+  return true;
+}
+
+function sortfunc(x,y) {
+  if (x.length < y.length) {
+    return -1;
+  }
+  if (x.length > y.length) {
+    return 1;
+  }
+  if (x < y) {
+    return -1;
+  }
+  if (x > y) {
+    return 1;
+  }
+  return 0;
+}
+
+function inputChanged(charString) {
+  findWords(wordlist, charString, function (goodWords) {
+    document.getElementById("results").textContent = goodWords.join("\n");
+    gw = goodWords;
+  })
+}
+
+function findWords(wordlist, charString, callback) {
+  var goodWords = [];
+  for (var i = 0; i < wordlist.length; i++) {
+    var word = wordlist[i];
+    if (wordInSet(word, charString)) {
+      goodWords.push(word);
+    }
+  }
+  goodWords.sort(sortfunc);
+  goodWords.reverse();
+  callback(goodWords);
+}
+
+(function () {
+  getWordlist(function (wl) {
+    wordlist = wl;
+    wordlistLoaded = true;
+    //findWords(wordlist, "rstlne", function (gw) {
+      //console.log(gw);
+      //goodWords = gw;
+    //});
+  });
+})()
