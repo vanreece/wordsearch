298640f897f073790bb89d12d03a486935aa61e2
Danny Krause <vanreece@gmail.com>
2019-08-29 09:55:37 -0700

    Moved board logic into board class

--- test_words_with_friends.py
+++ test_words_with_friends.py
@@ -1,92 +1,192 @@
 from words_with_friends import *
 import unittest
 
-def blank_board(size):
-    return [[' ' for x in range(size)] for x in range(size)]
 
-class TestWordsWithFriends(unittest.TestCase):
+def blank_board(size):
+    return [[" " for x in range(size)] for x in range(size)]
+
+
+class TestBoard(unittest.TestCase):
+    def test_board_constructors(self):
+        with self.assertRaisesRegex(
+            Exception, "Can't handle board with different length columns"
+        ):
+            Board([[" ", " "], [" "]])
+        with self.assertRaisesRegex(
+            Exception, "Can't handle board with column length 3 != column count 2"
+        ):
+            Board([[" ", " ", " "], [" ", " ", " "]])
+        self.assertEqual(1, Board([["A"]]).size())
+        self.assertEqual(2, Board([["A", "B"], ["A", "B"]]).size())
+        self.assertEqual(1, Board.fromsize(1).size())
+        self.assertEqual(2, Board.fromsize(2).size())
+        self.assertEqual(3, Board.fromsize(3).size())
 
     def test_board_text_translators(self):
-        current_board = blank_board(3)
-        current_board[1] = ['M', 'A', 'N']
-        text = board_to_text(current_board)
-        reboard = text_to_board(text)
-        self.assertEqual(current_board, reboard)
+        board = Board.fromsize(3)
+        board.set(0, 0, "M")
+        board.set(0, 1, "A")
+        board.set(0, 2, "N")
+        text = board.to_text()
+        self.assertEqual("M  \nA  \nN  ", text)
+        reboard = Board.fromstring(text)
+        self.assertEqual(board, reboard)
+
+    def test_set_and_get(self):
+        board = Board.fromsize(2)
+        board.set(0, 0, "M")
+        board.set(0, 1, "A")
+        board.set(1, 0, "D")
+        board.set(1, 1, "E")
+        self.assertEqual("M", board.get(0, 0))
+        self.assertEqual("A", board.get(0, 1))
+        self.assertEqual("D", board.get(1, 0))
+        self.assertEqual("E", board.get(1, 1))
+        with self.assertRaisesRegex(Exception, "Error, coordinates exceed board size"):
+            board.set(2, 2, "F")
+        with self.assertRaisesRegex(Exception, "Error, coordinates exceed board size"):
+            board.get(2, 2)
+
+    def test_columns_and_rows(self):
+        board = Board.fromsize(2)
+        board.set(0, 0, "M")
+        board.set(0, 1, "A")
+        board.set(1, 0, "D")
+        board.set(1, 1, "E")
+        self.assertEqual([["M", "D"], ["A", "E"]], board.rows())
+        self.assertEqual([["M", "A"], ["D", "E"]], board.columns())
+
+    def test_words(self):
+        board = Board.fromstring("MAN\nAN \nD  ")
+        self.assertEqual(sorted(["MAN", "AN", "AN", "MAD"]), sorted(board.words()))
 
 
+class TestWordsWithFriends(unittest.TestCase):
     def test_valid_move(self):
-        current_board = blank_board(3)
-        current_board[1] = ['M', 'A', 'N']
-        valid_move = blank_board(3)
-        valid_move[0][1] = 'Z'
-        valid_move[2][1] = 'P'
-        self.assertEqual((True, ""),
-                validate_move(current_board, valid_move, blank_board(3)))
-
+        board = Board.fromsize(3)
+        board.set(1, 0, "M")
+        board.set(1, 1, "A")
+        board.set(1, 2, "N")
+        # _M_
+        # _A_
+        # _N_
+        valid_move = []
+        valid_move.append((0, 1, "Z"))
+        valid_move.append((2, 1, "P"))
+        # _M_
+        # ZAP
+        # _N_
+        self.assertEqual((True, ""), board.validate_move(valid_move))
 
     def test_overlapping_letter(self):
-        current_board = blank_board(3)
-        current_board[1] = ['M', 'A', 'N']
-        overlapping_letter = blank_board(3)
-        overlapping_letter[1][1] = 'E'
-        self.assertEqual((False, "Tiles not occupying empty squares"),
-                validate_move(current_board, overlapping_letter, blank_board(3)))
-
+        board = Board.fromsize(3)
+        board.set(1, 0, "M")
+        board.set(1, 1, "A")
+        board.set(1, 2, "N")
+        # _M_
+        # _A_
+        # _N_
+        overlapping_letter = []
+        overlapping_letter.append((1, 1, "E"))
+        self.assertEqual(
+            (False, "Tiles not occupying empty squares"),
+            board.validate_move(overlapping_letter),
+        )
 
     def test_not_straight_line(self):
-        current_board = blank_board(3)
-        current_board[1] = ['M', 'A', 'N']
-        not_straight_line = blank_board(3)
-        not_straight_line[0][2] = 'O'
-        not_straight_line[2][1] = 'M'
-        self.assertEqual((False, "Tiles not in a straight line"),
-                validate_move(current_board, not_straight_line, blank_board(3)))
-
+        board = Board.fromsize(3)
+        board.set(1, 0, "M")
+        board.set(1, 1, "A")
+        board.set(1, 2, "N")
+        # _M_
+        # _A_
+        # _N_
+        not_straight_line = []
+        not_straight_line.append((0, 2, "O"))
+        not_straight_line.append((2, 1, "M"))
+        # _M_
+        # _AM
+        # ON_
+        self.assertEqual(
+            (False, "Tiles not in a straight line"),
+            board.validate_move(not_straight_line),
+        )
 
     def test_not_contiguous(self):
-        current_board = blank_board(3)
-        current_board[0] = ['M', 'A', 'N']
-        not_contiguous_single = blank_board(3)
-        not_contiguous_single[2][0] = 'A'
-        self.assertEqual((False, "Tiles not contiguous"),
-                validate_move(current_board, not_contiguous_single, blank_board(3)))
+        board = Board.fromsize(3)
+        board.set(0, 0, "M")
+        board.set(0, 1, "A")
+        board.set(0, 2, "N")
+        # M__
+        # A__
+        # N__
+        not_contiguous_single = [(2, 0, "A")]
+        # M_A
+        # A__
+        # N__
+        self.assertEqual(
+            (False, "Tiles not contiguous"), board.validate_move(not_contiguous_single)
+        )
+
+        not_contiguous_double_in_column = []
+        not_contiguous_double_in_column.append((1, 0, "A"))
+        not_contiguous_double_in_column.append((1, 2, "O"))
+        # MA_
+        # A__
+        # NO_
+        self.assertEqual(
+            (False, "Tiles not contiguous"),
+            board.validate_move(not_contiguous_double_in_column),
+        )
 
         current_board = blank_board(3)
-        current_board[0] = ['M', 'A', 'N']
-        not_contiguous_double_in_column = blank_board(3)
-        not_contiguous_double_in_column[1][0] = 'A'
-        not_contiguous_double_in_column[1][2] = 'O'
-        self.assertEqual((False, "Tiles not contiguous"),
-                validate_move(current_board, not_contiguous_double_in_column, blank_board(3)))
-
-        current_board = blank_board(3)
-        current_board[0][2] = 'M'
-        current_board[1][2] = 'A'
-        current_board[2][2] = 'N'
-        not_contiguous_double_in_row = blank_board(3)
-        not_contiguous_double_in_row[0][1] = 'A'
-        not_contiguous_double_in_row[2][1] = 'O'
-        self.assertEqual((False, "Tiles not contiguous"),
-                validate_move(current_board, not_contiguous_double_in_row, blank_board(3)))
-
+        current_board[0][2] = "M"
+        current_board[1][2] = "A"
+        current_board[2][2] = "N"
+        board = Board.fromsize(3)
+        board.set(0, 2, "M")
+        board.set(1, 2, "A")
+        board.set(2, 2, "N")
+        # ___
+        # ___
+        # MAN
+        not_contiguous_double_in_row = []
+        not_contiguous_double_in_row.append((0, 1, "A"))
+        not_contiguous_double_in_row.append((2, 1, "O"))
+        # ___
+        # A_O
+        # MAN
+        self.assertEqual(
+            (False, "Tiles not contiguous"),
+            board.validate_move(not_contiguous_double_in_row),
+        )
 
     def test_not_real_words(self):
-        current_board = blank_board(3)
-        current_board[1] = ['A', 'M', ' ']
-        bad_row = blank_board(3)
-        bad_row[1][0] = 'C'
-        self.assertEqual((False, "AC not found in wordlist"),
-                validate_move(current_board, bad_row, blank_board(3)))
-        bad_column = blank_board(3)
-        bad_column[0][2] = 'Z'
-        self.assertEqual((False, "AMZ not found in wordlist"),
-                validate_move(current_board, bad_column, blank_board(3)))
-
+        board = Board.fromsize(3)
+        board.set(1, 0, "A")
+        board.set(1, 1, "M")
+        # _A_
+        # _M_
+        # ___
+        bad_row = [(2, 0, "C")]
+        # _AC
+        # _M_
+        # ___
+        self.assertEqual(
+            (False, "AC not found in wordlist"), board.validate_move(bad_row)
+        )
+        bad_column = [(1, 2, "Z")]
+        # _A_
+        # _M_
+        # _Z_
+        self.assertEqual(
+            (False, "AMZ not found in wordlist"), board.validate_move(bad_column)
+        )
 
     def test_errors(self):
         with self.assertRaises(Exception):
-            validate_move([[' ']], [[' ', ' '],[' ', ' ']], [[' ', ' '],[' ', ' ']])
+            validate_move([[" "]], [[" ", " "], [" ", " "]], [[" ", " "], [" ", " "]])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
--- words_with_friends.py
+++ words_with_friends.py
@@ -2,67 +2,220 @@ import copy
 import os
 import sys
 import time
+import json
 
 words_with_friends_tiles = {
-  "A": {"number": 9, "points": 1},
-  "B": {"number": 2, "points": 4},
-  "C": {"number": 2, "points": 4},
-  "D": {"number": 5, "points": 2},
-  "E": {"number": 13, "points": 1},
-  "F": {"number": 2, "points": 4},
-  "G": {"number": 3, "points": 3},
-  "H": {"number": 4, "points": 3},
-  "I": {"number": 8, "points": 1},
-  "J": {"number": 1, "points": 10},
-  "K": {"number": 1, "points": 5},
-  "L": {"number": 4, "points": 2},
-  "M": {"number": 2, "points": 4},
-  "N": {"number": 5, "points": 2},
-  "O": {"number": 8, "points": 1},
-  "P": {"number": 2, "points": 4},
-  "Q": {"number": 1, "points": 10},
-  "R": {"number": 6, "points": 1},
-  "S": {"number": 5, "points": 1},
-  "T": {"number": 7, "points": 1},
-  "U": {"number": 4, "points": 2},
-  "V": {"number": 2, "points": 5},
-  "W": {"number": 2, "points": 4},
-  "X": {"number": 1, "points": 8},
-  "Y": {"number": 2, "points": 3},
-  "Z": {"number": 1, "points": 10},
-  ".": {"number": 2, "points": 0},
+    "A": {"number": 9, "points": 1},
+    "B": {"number": 2, "points": 4},
+    "C": {"number": 2, "points": 4},
+    "D": {"number": 5, "points": 2},
+    "E": {"number": 13, "points": 1},
+    "F": {"number": 2, "points": 4},
+    "G": {"number": 3, "points": 3},
+    "H": {"number": 4, "points": 3},
+    "I": {"number": 8, "points": 1},
+    "J": {"number": 1, "points": 10},
+    "K": {"number": 1, "points": 5},
+    "L": {"number": 4, "points": 2},
+    "M": {"number": 2, "points": 4},
+    "N": {"number": 5, "points": 2},
+    "O": {"number": 8, "points": 1},
+    "P": {"number": 2, "points": 4},
+    "Q": {"number": 1, "points": 10},
+    "R": {"number": 6, "points": 1},
+    "S": {"number": 5, "points": 1},
+    "T": {"number": 7, "points": 1},
+    "U": {"number": 4, "points": 2},
+    "V": {"number": 2, "points": 5},
+    "W": {"number": 2, "points": 4},
+    "X": {"number": 1, "points": 8},
+    "Y": {"number": 2, "points": 3},
+    "Z": {"number": 1, "points": 10},
+    ".": {"number": 2, "points": 0},
 }
 
-def text_to_board(text):
-    #Takes a string and makes it into a column major board
-    rows = text.split("\n")
-    column_counts = set([len(row) for row in rows])
-    if len(column_counts) != 1:
-        raise Exception(f"Can't handle board with different length rows, found these: {column_counts}")
-    row_length = column_counts.pop()
-    column_length = len(rows)
-    if column_length != row_length:
-        raise Exception(f"Can't handle board with row length {row_length} != column length {column_length}")
 
-    size = column_length
-    columns = []
-    for column_index in range(size):
-        column = []
-        for row_index in range(size):
-            column.append(rows[row_index][column_index])
-        columns.append(column)
-    return columns
+class Board(object):
+    # Idea here is to have a thing that can represent the board state, n x n,
+    # grab rows, columns, or direct access
+    def __init__(self, column_major_data):
+        column_lengths = set([len(column) for column in column_major_data])
+        if len(column_lengths) != 1:
+            raise Exception(
+                f"Can't handle board with different length columns, found these: {column_lengths}"
+            )
+        column_length = column_lengths.pop()
+        column_count = len(column_major_data)
+        if column_length != column_count:
+            raise Exception(
+                f"Can't handle board with column length {column_length} != column count {column_count}"
+            )
+        self._column_major_data = copy.deepcopy(column_major_data)
+        self._size = column_count
 
-def board_to_text(board):
-    #Takes a column major board and makes it into text
-    rows = list(zip(*board))
-    return "\n".join(["".join(row) for row in rows])
+    @classmethod
+    def fromsize(cls, size):
+        # Create empty size x size board
+        return cls([[" " for x in range(size)] for x in range(size)])
+
+    @classmethod
+    def fromstring(cls, text):
+        # Takes a string and makes it into a column major board
+        rows = text.split("\n")
+        column_counts = set([len(row) for row in rows])
+        if len(column_counts) != 1:
+            raise Exception(
+                f"Can't handle board with different length rows, found these: {column_counts}"
+            )
+        row_length = column_counts.pop()
+        column_length = len(rows)
+        if column_length != row_length:
+            raise Exception(
+                f"Can't handle board with row length {row_length} != column length {column_length}"
+            )
+
+        size = column_length
+        columns = []
+        for column_index in range(size):
+            column = []
+            for row_index in range(size):
+                column.append(rows[row_index][column_index])
+            columns.append(column)
+        return cls(columns)
+
+    def to_text(self):
+        return "\n".join(["".join(row) for row in self.rows()])
+
+    def set(self, x, y, character):
+        if x >= self._size or y >= self._size:
+            raise Exception("Error, coordinates exceed board size")
+        self._column_major_data[x][y] = character.upper()
+
+    def get(self, x, y):
+        if x >= self._size or y >= self._size:
+            raise Exception("Error, coordinates exceed board size")
+        return self._column_major_data[x][y]
+
+    def size(self):
+        return len(self._column_major_data)
+
+    def rows(self):
+        rows = []
+        for y in range(self.size()):
+            row = []
+            for x in range(self.size()):
+                row.append(self._column_major_data[x][y])
+            rows.append(row)
+        return rows
+
+    def columns(self):
+        return self._column_major_data
+
+    def words(self):
+        words = []
+        for line in self.rows() + self.columns():
+            words.extend([c for c in "".join(line).split(" ") if len(c) > 1])
+        return words
+
+    def __eq__(self, other):
+        return json.dumps(self.columns()) == json.dumps(other.columns())
+
+    def __ne__(self, other):
+        return json.dumps(self.columns()) != json.dumps(other.columns())
+
+    def validate_move(self, new_tiles):
+        # Proposed move is just a list of tuples: (X, Y, Letter)
+
+        # Needs:
+        # 1) Tiles must occupy empty squares
+        # 2) Tiles must be in a straight line
+        # 3) Tiles must be contiguous with each other (and existing tiles)
+        # 4) Tiles must spell valid words with all neighbors
+
+        # 1) Tiles must occupy empty squares
+        new_coordinates = set()
+        new_rows = set()
+        new_cols = set()
+        new_board = Board(self._column_major_data)
+        for x, y, letter in new_tiles:
+            if x >= self.size():
+                raise Exception(
+                    f"Invalid move, X coordinate ({x}) >= size ({self.size()})"
+                )
+            if y >= self.size():
+                raise Exception(
+                    f"Invalid move, Y coordinate ({y}) >= size ({self.size()})"
+                )
+            if self._column_major_data[x][y] != " ":
+                return (False, "Tiles not occupying empty squares")
+            new_rows.add(y)
+            new_cols.add(x)
+            new_coordinates.add((x, y))
+            new_board.set(x, y, letter)
+
+        # 2) Tiles must be in a straight line
+        new_row_count = len(new_rows)
+        new_col_count = len(new_cols)
+        new_count = len(new_tiles)
+        if new_row_count > 1 and new_col_count > 1:
+            return (False, "Tiles not in a straight line")
+
+        if new_count == 1:
+            # Single new character, must be contiguous on at least one side with existing tiles
+            x, y, _ = new_tiles[0]
+            found_neighbor = False
+            for neighbor_x, neighbor_y in neighbor_tiles(
+                x, y, self.size(), self.size()
+            ):
+                if self._column_major_data[neighbor_x][neighbor_y] != " ":
+                    found_neighbor = True
+            if not found_neighbor:
+                return (False, "Tiles not contiguous")
+        elif new_row_count > 1:
+            # New column of characters, must be contiguous with itself and existing tiles
+            # Also connected to existing tiles
+            min_row = min(new_rows)
+            max_row = max(new_rows)
+            new_col = new_cols.pop()
+            new_cols.add(new_col)
+            for row_index in range(min_row, max_row + 1):
+                test_x = new_col
+                test_y = row_index
+                if (
+                    self._column_major_data[test_x][test_y] == " "
+                    and (test_x, test_y) not in new_coordinates
+                ):
+                    return (False, "Tiles not contiguous")
+        elif new_col_count > 1:
+            # New row of characters, must be contiguous with itself and existing tiles
+            # Also connected to existing tiles
+            min_col = min(new_cols)
+            max_col = max(new_cols)
+            new_row = new_rows.pop()
+            new_rows.add(new_row)
+            for col_index in range(min_col, max_col + 1):
+                test_x = col_index
+                test_y = new_row
+                if (
+                    self._column_major_data[test_x][test_y] == " "
+                    and (test_x, test_y) not in new_coordinates
+                ):
+                    return (False, "Tiles not contiguous")
+
+        # Need to test *all* the words and make sure they're on the wordlist
+        for word in new_board.words():
+            if word not in wordset:
+                return (False, f"{word} not found in wordlist")
+
+        return (True, "")
+
 
 # W = Triple Word
 # L = Triple Letter
 # w = Double Word
 # l = Double Letter
-words_with_friends_full_board = text_to_board("""\
+words_with_friends_full_board = Board.fromstring(
+    """\
    W  L L  W   
   l  w   w  l  
  l  l     l  l 
@@ -78,12 +231,16 @@ W  L   w   L  W
  l  l     l  l 
   l  w   w  l  
    W  L L  W   \
-""")
+"""
+)
+
 
 def board_size(board):
     column_lengths = {len(column) for column in board}
     if len(column_lengths) != 1:
-        raise Exception(f"Can't handle board with different length columns, found these: {column_lengths}")
+        raise Exception(
+            f"Can't handle board with different length columns, found these: {column_lengths}"
+        )
     width = len(board)
     height = column_lengths.pop()
     return (width, height)
@@ -95,98 +252,15 @@ with open(wordlist_path, "r") as wordlist_fd:
     wordlist = [word[:-1].upper() for word in wordlist_fd]
 wordset = set(wordlist)
 
+
 def neighbor_tiles(x, y, width, height):
     neighbors = []
     if x > 0:
-        neighbors.append((x-1, y))
+        neighbors.append((x - 1, y))
     if x < width - 1:
-        neighbors.append((x+1, y))
+        neighbors.append((x + 1, y))
     if y > 0:
-        neighbors.append((x, y-1))
+        neighbors.append((x, y - 1))
     if y < height - 1:
-        neighbors.append((x, y+1))
+        neighbors.append((x, y + 1))
     return neighbors
-
-
-def validate_move(current_board, proposed_move, board_multiples):
-    # print(f"validate_move called with current_board:\n{board_to_text(current_board)}\nproposed_move: \n{board_to_text(proposed_move)}\nboard_multiples:\n{board_to_text(board_multiples)}")
-    if board_size(current_board) != board_size(proposed_move):
-        raise Exception(f"Can't apply different sized move to current board: {board_size(proposed_move)} != {board_size(current_board)}")
-    width, height = board_size(current_board)
-
-    # Needs:
-    # 1) Tiles must occupy empty squares
-    # 2) Tiles must be in a straight line
-    # 3) Tiles must be contiguous with each other (and existing tiles)
-    # 4) Tiles must spell valid words with all neighbors
-
-    # 1) Tiles must occupy empty squares
-    new_tiles = []
-    new_rows = set()
-    new_cols = set()
-    new_board = copy.deepcopy(current_board)
-    for x, column in enumerate(proposed_move):
-        for y, element in enumerate(column):
-            if element == ' ':
-                continue
-            if current_board[x][y] != ' ':
-                # Can't add tile {element} to {(x, y)}, occupied with {current_board[x][y]}
-                return (False, "Tiles not occupying empty squares")
-            new_tiles.append((x, y, element))
-            new_board[x][y] = element
-            new_rows.add(y)
-            new_cols.add(x)
-
-    # 2) Tiles must be in a straight line
-    new_row_count = len(new_rows)
-    new_col_count = len(new_cols)
-    new_count = len(new_tiles)
-    if new_row_count > 1 and new_col_count > 1:
-        return (False, "Tiles not in a straight line")
-
-    if new_count == 1:
-        # Single new character, must be contiguous on at least one side with existing tiles
-        x, y, _ = new_tiles[0]
-        found_neighbor = False
-        for neighbor_x, neighbor_y in neighbor_tiles(x, y, width, height):
-            if (current_board[neighbor_x][neighbor_y] != ' '):
-                found_neighbor = True
-        if not found_neighbor:
-            return (False, "Tiles not contiguous")
-    elif new_row_count > 1:
-        # New column of characters, must be contiguous with itself and existing tiles
-        # Also connected to existing tiles
-        min_row = min(new_rows)
-        max_row = max(new_rows)
-        new_col = new_cols.pop()
-        new_cols.add(new_col)
-        for row_index in range(min_row, max_row + 1):
-            test_x = new_col
-            test_y = row_index
-            if (current_board[test_x][test_y] == ' ' and
-                    proposed_move[test_x][test_y] == ' '):
-                return (False, "Tiles not contiguous")
-    elif new_col_count > 1:
-        # New row of characters, must be contiguous with itself and existing tiles
-        # Also connected to existing tiles
-        min_col = min(new_cols)
-        max_col = max(new_cols)
-        new_row = new_rows.pop()
-        new_rows.add(new_row)
-        for col_index in range(min_col, max_col + 1):
-            test_x = col_index
-            test_y = new_row
-            if (current_board[test_x][test_y] == ' ' and
-                    proposed_move[test_x][test_y] == ' '):
-                return (False, "Tiles not contiguous")
-
-    # Need to test *all* the words and make sure they're on the wordlist
-    for column in new_board:
-        # Find all the words, which are 1+ letters separated by spaces
-        words = [word for word in "".join(column).split(" ") if word]
-        for word in words:
-            if word not in wordset:
-                return (False, f"{word} not found in wordlist")
-
-
-    return (True, "")
